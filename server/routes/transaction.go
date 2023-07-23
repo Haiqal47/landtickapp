7f68ab345376b368347214a797ec2868453b044e
adrian wijaya <adr.wijayaa@gmail.com>
2023-07-23 23:10:10 +0700

    ketiga

--- server/routes/transaction.go
+++ server/routes/transaction.go
@@ -14,11 +14,11 @@ func TransactionRoutes(e *echo.Group) {
 
 	h := handlers.HandlerTransaction(transactionRepository)
 
-	e.GET("/transactions",  middleware.Auth(h.FindTransaction))
-	e.POST("/transaction",  middleware.Auth(h.CreateTransaction))
-	e.GET("/transaction/:id",  middleware.Auth(h.GetTransactionById))
+	e.GET("/transactions", middleware.Auth(h.FindTransaction))
+	e.POST("/transaction", middleware.Auth(h.CreateTransaction))
+	e.GET("/transaction/:id", middleware.Auth(h.GetTransactionById))
 	e.DELETE("/transaction/:id", middleware.Auth(h.DeleteTransaction))
-	e.GET("/transactionbyuser", middleware.Auth(h.GetTransactionByUser))
-	e.POST("/notification",h.Notification)
+	e.GET("/transactionbyuser", h.GetTransactionByUser)
+	e.POST("/notification", h.Notification)
 	e.POST("/payment", h.PaymentTransaction)
 }
